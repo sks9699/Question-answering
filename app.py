9eadf304f992a9d75f81d6d41d695842af611319
Monu kumar <sonugautam9699@gmail.com>
2024-06-13 17:52:14 +0545

    semi final

--- app.py
+++ app.py
@@ -1,37 +1,34 @@
-from fastapi import FastAPI, Form, Request, Response, File, Depends, HTTPException, status
+from fastapi import FastAPI, Form, Request, Response, File, HTTPException, status
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.encoders import jsonable_encoder
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import QAGenerationChain
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains import RetrievalQA, load_summarize_chain
 from langchain.text_splitter import TokenTextSplitter
 from langchain.docstore.document import Document
-from langchain.document_loaders import PyPDFLoader
 from langchain.prompts import PromptTemplate
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.summarize import load_summarize_chain
-from langchain.chains import RetrievalQA
-import os 
-import json
-import time
-import uvicorn
-import aiofiles
 from PyPDF2 import PdfReader
+import os
 import csv
+import aiofiles
+import json
+import uvicorn
 
 app = FastAPI()
 
+# Serve static files from the "static" directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Jinja2 templates directory
 templates = Jinja2Templates(directory="templates")
 
-os.environ["OPENAI_API_KEY"] = ""
+# Set OpenAI API key
+os.environ["OPENAI_API_KEY"] = "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"
 
-# Set file path
-# file_path = 'SDG.pdf'
-
+# Function to count pages in a PDF
 def count_pdf_pages(pdf_path):
     try:
         pdf = PdfReader(pdf_path)
@@ -40,9 +37,8 @@ def count_pdf_pages(pdf_path):
         print("Error:", e)
         return None
 
+# Function to process a PDF file
 def file_processing(file_path):
-
-    # Load data from PDF
     loader = PyPDFLoader(file_path)
     data = loader.load()
 
@@ -52,9 +48,9 @@ def file_processing(file_path):
         question_gen += page.page_content
         
     splitter_ques_gen = TokenTextSplitter(
-        model_name = 'gpt-3.5-turbo',
-        chunk_size = 10000,
-        chunk_overlap = 200
+        model_name='gpt-3.5-turbo',
+        chunk_size=10000,
+        chunk_overlap=200
     )
 
     chunks_ques_gen = splitter_ques_gen.split_text(question_gen)
@@ -62,25 +58,22 @@ def file_processing(file_path):
     document_ques_gen = [Document(page_content=t) for t in chunks_ques_gen]
 
     splitter_ans_gen = TokenTextSplitter(
-        model_name = 'gpt-3.5-turbo',
-        chunk_size = 1000,
-        chunk_overlap = 100
+        model_name='gpt-3.5-turbo',
+        chunk_size=1000,
+        chunk_overlap=100
     )
 
-
-    document_answer_gen = splitter_ans_gen.split_documents(
-        document_ques_gen
-    )
+    document_answer_gen = splitter_ans_gen.split_documents(document_ques_gen)
 
     return document_ques_gen, document_answer_gen
 
+# Function to set up LLM pipeline
 def llm_pipeline(file_path):
-
     document_ques_gen, document_answer_gen = file_processing(file_path)
 
     llm_ques_gen_pipeline = ChatOpenAI(
-        temperature = 0.3,
-        model = "gpt-3.5-turbo"
+        temperature=0.3,
+        model="gpt-3.5-turbo"
     )
 
     prompt_template = """
@@ -100,7 +93,7 @@ def llm_pipeline(file_path):
 
     PROMPT_QUESTIONS = PromptTemplate(template=prompt_template, input_variables=["text"])
 
-    refine_template = ("""
+    refine_template = """
     You are an expert at creating practice questions based on coding material and documentation.
     Your goal is to help a coder or programmer prepare for a coding test.
     We have received some practice questions to a certain extent: {existing_answer}.
@@ -114,18 +107,19 @@ def llm_pipeline(file_path):
     If the context is not helpful, please provide the original questions.
     QUESTIONS:
     """
-    )
 
     REFINE_PROMPT_QUESTIONS = PromptTemplate(
         input_variables=["existing_answer", "text"],
         template=refine_template,
     )
 
-    ques_gen_chain = load_summarize_chain(llm = llm_ques_gen_pipeline, 
-                                            chain_type = "refine", 
-                                            verbose = True, 
-                                            question_prompt=PROMPT_QUESTIONS, 
-                                            refine_prompt=REFINE_PROMPT_QUESTIONS)
+    ques_gen_chain = load_summarize_chain(
+        llm=llm_ques_gen_pipeline,
+        chain_type="refine",
+        verbose=True,
+        question_prompt=PROMPT_QUESTIONS,
+        refine_prompt=REFINE_PROMPT_QUESTIONS
+    )
 
     ques = ques_gen_chain.run(document_ques_gen)
 
@@ -138,33 +132,21 @@ def llm_pipeline(file_path):
     ques_list = ques.split("\n")
     filtered_ques_list = [element for element in ques_list if element.endswith('?') or element.endswith('.')]
 
-    answer_generation_chain = RetrievalQA.from_chain_type(llm=llm_answer_gen, 
-                                                chain_type="stuff", 
-                                                retriever=vector_store.as_retriever())
+    answer_generation_chain = RetrievalQA.from_chain_type(
+        llm=llm_answer_gen,
+        chain_type="stuff",
+        retriever=vector_store.as_retriever()
+    )
 
     return answer_generation_chain, filtered_ques_list
 
-
-
-
-# Answer each question and save to a file
-# for question in question_list:
-#     print("Question: ", question)
-#     answer = answer_gen_chain.run(question)
-#     print("Answer: ", answer)
-#     print("--------------------------------------------------\\n\\n")
-#     # Save answer to file
-#     with open("answers.txt", "a") as f:
-#         f.write("Question: " + question + "\\n")
-#         f.write("Answer: " + answer + "\\n")
-#         f.write("--------------------------------------------------\\n\\n")
-
-def get_csv (file_path):
+# Function to generate CSV file with Q&A
+def get_csv(file_path):
     answer_generation_chain, ques_list = llm_pipeline(file_path)
     base_folder = 'static/output/'
     if not os.path.isdir(base_folder):
         os.mkdir(base_folder)
-    output_file = base_folder+"QA.csv"
+    output_file = base_folder + "QA.csv"
     with open(output_file, "w", newline="", encoding="utf-8") as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(["Question", "Answer"])  # Writing the header row
@@ -179,10 +161,12 @@ def get_csv (file_path):
             csv_writer.writerow([question, answer])
     return output_file
 
+# Endpoint to render index.html
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+# Endpoint to upload a PDF file
 @app.post("/upload")
 async def chat(request: Request, pdf_file: bytes = File(), filename: str = Form(...)):
     base_folder = 'static/docs/'
@@ -192,20 +176,17 @@ async def chat(request: Request, pdf_file: bytes = File(), filename: str = Form(
 
     async with aiofiles.open(pdf_filename, 'wb') as f:
         await f.write(pdf_file)
-    # page_count = count_pdf_pages(pdf_filename)
-    # if page_count > 5:
-    #     return Response(jsonable_encoder(json.dumps({"msg": 'error'})))
-    response_data = jsonable_encoder(json.dumps({"msg": 'success',"pdf_filename": pdf_filename}))
-    res = Response(response_data)
-    return res
 
+    response_data = {"msg": 'success', "pdf_filename": pdf_filename}
+    return response_data
 
+# Endpoint to analyze the uploaded PDF file and generate CSV
 @app.post("/analyze")
 async def chat(request: Request, pdf_filename: str = Form(...)):
     output_file = get_csv(pdf_filename)
-    response_data = jsonable_encoder(json.dumps({"output_file": output_file}))
-    res = Response(response_data)
-    return res
+    response_data = {"output_file": output_file}
+    return response_data
 
+# Main entry point to run the FastAPI application
 if __name__ == "__main__":
     uvicorn.run("app:app", host='0.0.0.0', port=8000, reload=True)
